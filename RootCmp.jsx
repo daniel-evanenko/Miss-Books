04dfb0cf03ffe938ab982a9844c7cd40d9ddf5c6
daniel <daniel.evanenko@gmail.com>
2025-02-05 12:48:02 +0200

    init repo - MissBooks

---
+++ RootCmp.jsx
@@ -0,0 +1,36 @@
+const Router = ReactRouterDOM.HashRouter
+const { Routes, Route, Navigate } = ReactRouterDOM
+
+
+import { Team } from "./cmps/AboutCmps/Team.jsx"
+import { Vision } from "./cmps/AboutCmps/Vision.jsx"
+import { AppHeader } from "./cmps/AppHeader.jsx"
+import { NotFound } from "./cmps/NotFound.jsx"
+import { AboutUs } from "./pages/AboutUs.jsx"
+import { CarDetails } from "./pages/CarDetails.jsx"
+import { CarEdit } from "./pages/CarEdit.jsx"
+import { CarIndex } from "./pages/CarIndex.jsx"
+import { HomePage } from "./pages/HomePage.jsx"
+
+
+export function App() {
+
+    return (
+        <Router>
+            <section className="app">
+                <AppHeader />
+                <main className="main-layout">
+                    <Routes>
+                        <Route path="/" element={<Navigate to="/home" />} />
+                        <Route path="/home" element={<HomePage />} />
+                        <Route path="/about" element={<AboutUs />} >
+                            <Route path="/about/team" element={<Team />} />
+                            <Route path="/about/vision" element={<Vision />} />
+                        </Route>
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </main>
+            </section>
+        </Router>
+    )
+}
---
+++ pages/HomePage.jsx
@@ -0,0 +1,10 @@
+
+export function HomePage() {
+
+    return (
+        <section className="home">
+            <h1 >Books R Us!</h1>
+            <img src="../assets/img/react.png" alt="hero-image" />
+        </section>
+    )
+}
