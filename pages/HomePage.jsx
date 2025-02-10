09ea6d49e737bfe0a64ce3200ea56e0a48635797
daniel <daniel.evanenko@gmail.com>
2025-02-10 19:59:59 +0200

    debug

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -3,7 +3,7 @@ export function HomePage() {
   return (
     <section className="home">
       <h1 >Books R Us!</h1>
-      <img src="/assets/img/missBooks.png" alt="bookShop-image"/>
+      <img src="/assets/img/missBooks.png" alt="Miss Books" />
     </section>
   )
 }
